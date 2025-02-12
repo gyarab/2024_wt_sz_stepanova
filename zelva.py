669f0f0a5972e567fea4ad595d7a83c07a6dc5c5
KiraStepanova <148944110+KiraStepanova@users.noreply.github.com>
2025-02-12 21:43:46 +0100

    Create zelva.py

---
+++ zelva.py
@@ -0,0 +1,25 @@
+from turtle import forward, left, shape, exitonclick
+
+from math import sqrt
+delka = sqrt(2)*50
+
+forward(100)
+
+forward(100)
+shape('turtle')
+left(90)
+forward(100)
+left(45)
+forward(delka)
+left(90)
+forward(delka)
+left(45)
+forward(100)
+left(135)
+forward(delka*2)
+left(135)
+forward(100)
+left(135)
+forward(delka*2)
+
+exitonclick
